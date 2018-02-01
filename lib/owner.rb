e2f0cdaf5d228579cc4fe3d6feec8238f5221b30
Onur Eker <yonureker@gmail.com>
2018-02-01 16:28:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,4 +75,22 @@ attr_reader :species
       end
     end
 
+    def list_pets
+      fish_count = self.pets[:fishes].count
+      dog_count = self.pets[:dogs].count
+      cat_count = self.pets[:cats].count
+
+      if dog_count > 0
+        dog_p = "dogs"
+      else dog_p = "dog"
+      end
+
+      if cat_count > 0 
+        cat_p = "cats"
+      else cat_p = "cat"
+      end
+
+      "I have #{fish_count} fish, #{dog_count} #{dog_p}, and #{cat_count} #{cat_p}"
+    end
+
 end
